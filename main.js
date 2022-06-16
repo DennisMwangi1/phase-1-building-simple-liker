a7b6f3715bf40ed985c0c33d7c690ca410bbfe6b
Dennis Mwangi <dennis.kamau@student.moringaschool.com>
2022-06-16 09:51:51 +0300

    commit tree 1fc1c5f9195bd3538a3d80e4f97ec1118f11660b

--- main.js
+++ main.js
@@ -3,37 +3,35 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-const likers = document.querySelectorAll('span.like-glyph')
-likers.forEach(liker => {
-  liker.addEventListener('click',()=>{
-    if (liker.innerText === FULL_HEART) {
+document.addEventListener('DOMContentLoaded',()=>{
+  const likers = document.querySelectorAll('span.like-glyph')
+  likers.forEach(liker=>{
+    liker.addEventListener('click',()=>{
+      if (liker.innerText === FULL_HEART ) {
         liker.classList.remove('activated-heart')
         liker.innerText = EMPTY_HEART
-    }else{
-    mimicServerCall()
-    .then(()=>{
-      liker.innerText = FULL_HEART
-      liker.classList.add('activated-heart')
-      
-      
+      }else{
+        mimicServerCall()
+        .then(()=>{
+          liker.innerText = FULL_HEART
+          liker.classList.add('activated-heart')
+        })
+        .catch(()=>{
+          let error = document.getElementById('modal')
+          error.classList.remove('hidden')
+          let message = document.getElementById('modal-message')
+          message.innerText = "Random server error. Try again."
+          setTimeout(()=>{
+            error.classList.add('hidden')
+          },3000)
+        })
+      }
     })
-  
-    .catch(()=>{
-      let error = document.getElementById('modal')
-      error.classList.remove('hidden')
-      let message = document.getElementById('modal-message')
-      message.innerText = "Random server error. Try again."
-      setTimeout(()=>{
-        error.classList.add('hidden')
-     },3000)
-  
-     
-   
-   
   })
-}})
-});
 
+
+
+})
 
 
 
